d8c7ac755924766d0c10cfa8700a127986d357d0
codephilosopher <codephilosopher000@gmail.com>
2022-06-19 22:10:58 +0530

    some changes

--- src/main/java/alternate/browser/settings02/Main.java
+++ src/main/java/alternate/browser/settings02/Main.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Main {
-	public static String browser = "safari";
+	public static String browser = "chrome";
 	public static WebDriver driver ;
 	public static void main(String[] args) {
 		if (browser.equals("firefox")) {
