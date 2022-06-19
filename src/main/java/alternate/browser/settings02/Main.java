4d048639812237e0a3ed971ac717f771fd82a909
codephilosopher <60250242+codephilosopher@users.noreply.github.com>
2022-06-19 16:27:13 +0530

    changes in browser name to edge

--- src/main/java/alternate/browser/settings02/Main.java
+++ src/main/java/alternate/browser/settings02/Main.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Main {
-	public static String browser = "explorer";
+	public static String browser = "edge";
 	public static WebDriver driver ;
 	public static void main(String[] args) {
 		if (browser.equals("firefox")) {
