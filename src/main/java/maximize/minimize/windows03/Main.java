3024765b55831321376fff15db681f53c3d170df
codephilosopher <codephilosopher000@gmail.com>
2022-06-19 22:54:39 +0530

    added selenium code for maximise and minimize screen

---
+++ src/main/java/maximize/minimize/windows03/Main.java
@@ -0,0 +1,35 @@
+package maximize.minimize.windows03;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Main {
+
+	public static String browser = "edge";
+	public static WebDriver driver ;
+	public static void main(String[] args) {
+		if (browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().driverVersion("0.31.0").setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else {
+			System.err.println("supported browser not found!");
+			System.exit(0);
+		}
+		
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		driver.manage().window().minimize();
+		driver.quit();
+	}
+
+}
