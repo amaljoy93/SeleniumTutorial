c8da087846b3db934782789a1f9905aa098d485a
codephilosopher <codephilosopher000@gmail.com>
2022-06-20 13:42:21 +0530

    intermediate commit, this commit dont count

--- src/main/java/browserconfiguration/settings01/Main.java
+++ src/main/java/browserconfiguration/settings01/Main.java
@@ -1,12 +1,17 @@
 package browserconfiguration.settings01;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\prade\\OneDrive\\Documents\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
 		driver.quit();
 	}
